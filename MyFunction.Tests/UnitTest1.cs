dff6d3ec189b67c34a521b7740c68ecbce9710c5
Tommy Ly <tommy.ly@esendex.com>
2019-07-08 11:17:47 +0100

    updates csproj

--- MyFunction.Tests/UnitTest1.cs
+++ MyFunction.Tests/UnitTest1.cs
@@ -15,5 +15,11 @@ namespace MyFunction.Tests
         {
             Assert.False(false);
         }
+
+        [Fact]
+        public void Test3()
+        {
+            Assert.False(false);
+        }
     }
 }
