1e81246cafecef5e56648f9eef7145bbf5b030d3
Tommy Ly <tommy.ly@esendex.com>
2019-07-09 15:43:19 +0100

    changes

--- MyFunction/Function1.cs
+++ MyFunction/Function1.cs
@@ -14,7 +14,7 @@ namespace MyFunction
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("I'm now a changed man 7");
+            log.LogInformation("I'm now a changed man 8");
 
             return new OkResult	();
         }
